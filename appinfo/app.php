3a25cf1e137d322882dd268496271ad33ae4526c
Julius Haertl <jus@bitgrid.net>
2017-01-10 12:32:37 +0100

    Add cachebuster for theming

--- appinfo/app.php
+++ appinfo/app.php
@@ -21,7 +21,14 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-$linkToCSS = \OC::$server->getURLGenerator()->linkToRoute('direct_menu.App.stylesheet');
+
+$linkToCSS = \OC::$server->getURLGenerator()->linkToRoute(
+	'direct_menu.App.stylesheet',
+	[
+		'v' => \OC::$server->getConfig()->getAppValue('theming', 'cachebuster', '0'),
+	]
+);
+
 \OCP\Util::addHeader(
 	'link',
 	[
